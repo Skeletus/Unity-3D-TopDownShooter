056c707555dbd721c0c61139e5c0eeba661f72c1
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-05-14 12:07:07 -0500

    using input system in player movement  scirpt

--- Game3DTopDownShooter/Assets/PlayerMovement.cs
+++ Game3DTopDownShooter/Assets/PlayerMovement.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerControls controls;
+
+    public Vector2 moveInput;
+    public Vector2 aimInput;
+
+    private void Awake()
     {
-        
+        controls = new PlayerControls();
+
+        controls.Character.Movement.performed += context => moveInput = context.ReadValue<Vector2>();
+        controls.Character.Movement.canceled += context => moveInput = Vector2.zero;
+
+        controls.Character.Aim.performed += context => aimInput = context.ReadValue<Vector2>();
+        controls.Character.Aim.canceled += context => aimInput = Vector2.zero;
+        //controls.Character.Fire.performed += context => Shoot();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Shoot()
     {
-        
+        Debug.Log("Shot");
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
     }
 }
