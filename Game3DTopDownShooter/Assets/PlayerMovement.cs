6f46a16788583cc5e102bbad6af8a85457bbc33e
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-05-15 19:27:34 -0500

    Added movemnt funciton in player movement script

--- Game3DTopDownShooter/Assets/PlayerMovement.cs
+++ Game3DTopDownShooter/Assets/PlayerMovement.cs
@@ -6,9 +6,14 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerControls controls;
+    private CharacterController characterController;
 
-    public Vector2 moveInput;
-    public Vector2 aimInput;
+    [Header("Movement info")]
+    [SerializeField] private float walkSpeed;
+    private Vector3 movementDirection;
+
+    private Vector2 moveInput;
+    private Vector2 aimInput;
 
     private void Awake()
     {
@@ -20,6 +25,26 @@ public class PlayerMovement : MonoBehaviour
         controls.Character.Aim.performed += context => aimInput = context.ReadValue<Vector2>();
         controls.Character.Aim.canceled += context => aimInput = Vector2.zero;
         //controls.Character.Fire.performed += context => Shoot();
+    }
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        ApplyMovement();
+    }
+
+    private void ApplyMovement()
+    {
+        movementDirection = new Vector3(moveInput.x, 0, moveInput.y);
+
+        if (movementDirection.magnitude > 0)
+        {
+            characterController.Move(movementDirection * Time.deltaTime * walkSpeed);
+        }
     }
 
     private void Shoot()
