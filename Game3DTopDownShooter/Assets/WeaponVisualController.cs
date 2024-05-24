298058d538dab39fb54d9c693ea2e9043e9edf81
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-05-23 21:05:14 -0500

    added weapon visualizer controller script

--- Game3DTopDownShooter/Assets/WeaponVisualController.cs
+++ Game3DTopDownShooter/Assets/WeaponVisualController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class WeaponVisualController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform[] gunsList;
+
+    [SerializeField] private Transform pistol;
+    [SerializeField] private Transform revolver;
+    [SerializeField] private Transform autoRifle;
+    [SerializeField] private Transform shotgun;
+    [SerializeField] private Transform rifle;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1)) // 1 in keyboard
+        {
+            SwitchOn(pistol);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) // 2 in keyboard
+        {
+            SwitchOn(revolver);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) // 3 in keyboard
+        {
+            SwitchOn(autoRifle);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4)) // 4 in keyboard
+        {
+            SwitchOn(shotgun);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5)) // 5 in keyboard
+        {
+            SwitchOn(rifle);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SwitchOn(Transform gun)
     {
-        
+        SwitchOffGuns(); // desactivate all guns
+        // activate given gun
+        gun.gameObject.SetActive(true);
+    }
+
+    private void SwitchOffGuns()
+    {
+        for(int i = 0; i < gunsList.Length; i++)
+        {
+            gunsList[i].gameObject.SetActive(false);
+        }
     }
 }
