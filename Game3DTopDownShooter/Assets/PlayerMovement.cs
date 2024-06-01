bf82e9e8ffaa45ee078951064a8f434580983258
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-06-01 18:05:23 -0500

    updated script fixed aim transfrom position

--- Game3DTopDownShooter/Assets/PlayerMovement.cs
+++ Game3DTopDownShooter/Assets/PlayerMovement.cs
@@ -55,7 +55,7 @@ public class PlayerMovement : MonoBehaviour
             lookingDirection.Normalize();
 
             transform.forward = lookingDirection;
-            aim.position = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
+            aim.position = new Vector3(hitInfo.point.x, transform.position.y + 1, hitInfo.point.z);
         }
     }
 
