7a596636eddab9ec650a02a54063f5b0d962a41d
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-06-09 13:45:34 -0500

    created script and attached to left hand game objects

---
+++ Game3DTopDownShooter/Assets/LeftHandTargetTransform.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class LeftHandTargetTransform : MonoBehaviour
+{
+    // we need this script to detect game object
+    // we detecting left hand targets position and rotation
+}
