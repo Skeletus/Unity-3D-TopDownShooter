ae025deb9d9e52250e7882844933d852e4e3de74
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-06-23 14:46:00 -0500

    updated player weapon controller - added ondrawgizmos

--- Game3DTopDownShooter/Assets/Scripts/PlayerWeaponController.cs
+++ Game3DTopDownShooter/Assets/Scripts/PlayerWeaponController.cs
@@ -10,6 +10,9 @@ public class PlayerWeaponController : MonoBehaviour
     [SerializeField] private float bulletSpeed;
     [SerializeField] private Transform gunPoint;
 
+    [SerializeField] private Transform weaponHolder;
+    [SerializeField] private Transform aim;
+
     private void Start()
     {
         playerControls = GetComponent<Player>();
@@ -23,5 +26,14 @@ public class PlayerWeaponController : MonoBehaviour
         Destroy(newBullet, 10);
 
         GetComponentInChildren<Animator>().SetTrigger("Fire");
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(weaponHolder.position, weaponHolder.position + weaponHolder.forward * 25);
+
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawLine(gunPoint.position, gunPoint.position + gunPoint.forward * 25);
     }
 }
