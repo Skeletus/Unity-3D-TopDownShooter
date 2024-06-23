9af50771b3224e0b6d0fbeb195652790c422c5d0
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-06-22 19:03:17 -0500

    updated bullet script

--- Game3DTopDownShooter/Assets/Scripts/Bullet.cs
+++ Game3DTopDownShooter/Assets/Scripts/Bullet.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Rigidbody rb => GetComponent<Rigidbody>();
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        rb.constraints = RigidbodyConstraints.FreezeAll;
     }
 }
